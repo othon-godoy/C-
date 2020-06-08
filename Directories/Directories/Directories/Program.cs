a63e43512772060e5b849fe46dc183c28be550ee
Othon Godoy <qa.01@conlifebank.com.br>
2020-06-08 16:41:56 -0300

    C#

---
+++ Directories/Directories/Directories/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Directory e DirectoryInfo
+
+            /*
+            string path = @"C:\Users\moneto\Documents\Cursos\C#\Directories";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                // var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+
+                Console.WriteLine("Folders: ");
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);               
+
+                Console.WriteLine("Files: ");
+                foreach (string f in files)
+                {
+                    Console.WriteLine(f);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            */
+
+
+            // ### Path
+            string path = @"C:\Users\moneto\Documents\Cursos\C#\Directories\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar:" + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator:" + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName:" + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension:" + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension:" + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath:" + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath:" + Path.GetTempPath());
+        }
+    }
+}
---
+++ Files/Files/Files/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ### File, FileInfo e IOException
+
+            /*
+            string sourcePath = @"C:\Users\moneto\Documents\Cursos\C#\Files\file1.txt";
+            string targetPath = @"C:\Users\moneto\Documents\Cursos\C#\Files\file2.txt";            
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+            */
+
+
+            // ### FileStream e StreamReader
+
+            /*
+            string path = @"C:\Users\moneto\Documents\Cursos\C#\Files\file1.txt";
+
+            // FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                // fs = new FileStream(path, FileMode.Open);
+                // sr = new StreamReader(fs);
+                sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            } 
+            finally
+            {
+                if (sr != null) sr.Close();
+                // if (fs != null) fs.Close();
+            }
+            */
+
+
+            // ### Bloco using
+
+            /*
+            string path = @"C:\Users\moneto\Documents\Cursos\C#\Files\fileX.txt";
+
+            try
+            {
+                
+                //using (FileStream fs = new FileStream(path, FileMode.Open))
+                //{
+                //    using (StreamReader sr = new StreamReader(fs))
+                //    {
+                //        while (!sr.EndOfStream)
+                //        {
+                //            string line = sr.ReadLine();
+                //            Console.WriteLine(line);
+                //        }
+                //    }
+                //}                
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            } 
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }  
+            */
+
+            string sourcePath = @"C:\Users\moneto\Documents\Cursos\C#\Files\file1.txt";
+            string targetPath = @"C:\Users\moneto\Documents\Cursos\C#\Files\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
