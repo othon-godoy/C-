1a01b484dcafb9bacfa4d8c93ae5e9748529fda1
Othon Godoy <qa.01@conlifebank.com.br>
2020-06-09 17:53:55 -0300

    C#

---
+++ Generics/Generics/Generics/Program.cs
@@ -0,0 +1,145 @@
+﻿using Generics.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Principal;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            PrintService<string> printService = new PrintService<string>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+
+            Console.WriteLine("First: " + printService.First());
+            */
+
+            /*
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+            */
+
+            /*
+            Client a = new Client { Email = "maria@email.com", Name = "Maria" };
+            Client b = new Client { Email = "alex@email.com", Name = "Alex" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            */
+
+            /*
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Comp"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }  
+            */
+
+            /*
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            PrintCollection(a);
+
+            // union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+
+            PrintCollection(c);
+
+            // intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+
+            PrintCollection(d);
+
+            // diference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+
+            PrintCollection(e);
+            */
+
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@email.com";
+            cookies["phone"] = "98765432101";
+            cookies["phone"] = "10123456789";
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no email key");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("All cookies: ");
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+            foreach (var item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+
+        static void PrintCollection<T> (IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
