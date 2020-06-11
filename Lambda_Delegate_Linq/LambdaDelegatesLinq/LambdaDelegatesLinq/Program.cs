bb72f6a500c48f268ff6dfe50aa2c3d1e3cbf91b
Othon Godoy <othon.dev@outlook.com>
2020-06-10 22:59:28 -0300

    C#

--- Lambda_Delegate_Linq/LambdaDelegatesLinq/LambdaDelegatesLinq/Program.cs
+++ Lambda_Delegate_Linq/LambdaDelegatesLinq/LambdaDelegatesLinq/Program.cs
@@ -197,8 +197,73 @@ namespace LambdaDelegatesLinq
                 new Product() { Id = 3, Name = "TV", Price = 1700.0, Category = c3 },
                 new Product() { Id = 4, Name = "Notebook", Price = 1300.0, Category = c2 },
                 new Product() { Id = 5, Name = "Saw", Price = 80.0, Category = c1 }
-            };            
+            };
 
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.0
+                select p;
+
+            Print("Tier 1 and Price < 900: ", r1);
+
+
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
+
+            Print("Names of products from tools", r2);
+
+
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name
+                };
+
+            Print("Names started with 'C' and anonymous object", r3);
+
+
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
+
+            Print("Tier 1 order by price then by name", r4);
+
+
+            var r5 =
+                (from p in r4
+                 select p)
+                .Skip(2)
+                .Take(4);
+
+            Print("Tier 1 order by price then by name skip 2 take 4", r5);
+
+
+            var r16 =
+                from p in products
+                group p by p.Category;
+
+            foreach (IGrouping<Category, Product> group in r16)
+            {
+                Console.WriteLine("Category " + group.Key.Name + ": ");
+
+                foreach (Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+
+                Console.WriteLine();
+            }
+
+            /*
             var r1 = products
                 .Where(p => p.Category.Tier == 1 && p.Price < 900);
 
@@ -255,6 +320,67 @@ namespace LambdaDelegatesLinq
                 .SingleOrDefault();
 
             Console.WriteLine("Single or default: " + r8);
+
+
+            var r10 = products
+                .Max(p => p.Price);
+
+            Console.WriteLine("Max price: " + r10);
+
+
+            var r11 = products
+               .Min(p => p.Price);
+
+            Console.WriteLine("Max price: " + r11);
+
+
+            var r12 = products
+                .Where(p => p.Category.Id == 1)
+                .Sum(p => p.Price);
+
+            Console.WriteLine("Category 1 Sum prices: " + r12);
+
+
+            var r13 = products
+                .Where(p => p.Category.Id == 1)
+                .Average(p => p.Price);
+
+            Console.WriteLine("Category 1 Average prices: " + r13);
+
+
+            var r14 = products
+                .Where(p => p.Category.Id == 5)
+                .Select(p => p.Price)
+                .DefaultIfEmpty(0.0)
+                .Average();
+
+            Console.WriteLine("Category 5 Average prices: " + r14);
+
+
+            var r15 = products
+                .Where(p => p.Category.Id == 1)
+                .Select(p => p.Price)
+                .Aggregate(0.0, (x, y) => x + y);
+
+            Console.WriteLine("Category 1 aggregate sum: " + r15);
+
+
+            Console.WriteLine();
+
+            var r16 = products.GroupBy(p => p.Category);
+
+            foreach (IGrouping<Category, Product> group in r16)
+            {
+                Console.WriteLine("Category " + group.Key.Name + ": ");
+
+                foreach (Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+
+                Console.WriteLine();
+            }
+            */
         }
 
         static void Print<T>(string message, IEnumerable<T> collection)
