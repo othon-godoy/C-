ea6ba9cd4619ba0b541f8a6555b6bea3207baefd
Othon Godoy <othon.dev@outlook.com>
2020-06-13 19:47:54 -0300

    C#

---
+++ Projetos/SalesWeb/SalesWeb/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SalesWeb.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public string Message { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Projetos/SalesWeb/SalesWeb/Services/SellerService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWeb.Data;
+using SalesWeb.Models;
+using SalesWeb.Services.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWeb.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebContext _context;
+
+        public SellerService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+
+            _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+
+            _context.Seller.Remove(obj);
+
+            _context.SaveChanges();
+        }
+    }
+}
